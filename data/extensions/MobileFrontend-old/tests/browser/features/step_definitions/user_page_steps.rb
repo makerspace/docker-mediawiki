afbce03a8bb0f0935864833ee6a55818d79a20b2
Johan Kristensen <5310683+DevJohan@users.noreply.github.com>
2019-02-04 03:52:59 +0000

    Remove old MobileFrontend extension

--- data/extensions/MobileFrontend-old/tests/browser/features/step_definitions/user_page_steps.rb
+++
@@ -1,8 +0,0 @@
-Then(/^I should be on my user page$/) do
-  on(UserPage) do |page|
-    page.wait_until do
-      page.heading_element.when_present
-    end
-    expect(page.heading_element).to be_visible
-  end
-end
